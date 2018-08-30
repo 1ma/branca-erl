1739fee061b7a4b89e9247ec67084652e5e270cd
Marcel Hernandez <e6990620@gmail.com>
2018-08-30 17:54:38 +0200

    complete implementation

--- src/branca.erl
+++ src/branca.erl
@@ -6,11 +6,17 @@
 %%====================================================================
 %% API functions
 %%====================================================================
-encode(PlainText, Secret) -> transcode(PlainText, Secret, 123).
+encode(PlainText, Secret) ->
+  Nonce = libsodium_randombytes:buf(12),
+  Header = <<16#BA, (erlang:system_time(seconds)):32, Nonce/binary>>,
+  Payload = libsodium_crypto_aead_chacha20poly1305:ietf_encrypt(PlainText, Header, Nonce, Secret),
+  branca_base62:encode(<<Header/binary, Payload/binary>>).
 
-decode(CipherText, Secret) -> plaintext.
+decode(CipherText, Secret) ->
+  <<Header:17/bytes, Payload/binary>> = branca_base62:decode(CipherText),
+  <<16#BA, _:4/bytes, Nonce:12/bytes>> = Header,
+  libsodium_crypto_aead_chacha20poly1305:ietf_decrypt(Payload, Header, Nonce, Secret).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-transcode(Data, SrcBase, DstBase) -> transcoded.
