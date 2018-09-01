e27799caeeea6314203b396ca830da0086b62098
Marcel Hernandez <e6990620@gmail.com>
2018-09-01 22:51:53 +0200

    branca property

---
+++ test/prop_branca.erl
@@ -0,0 +1,20 @@
+-module(prop_branca).
+-import(branca, [encode/2, decode/2]).
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_symmetric_encryption() ->
+  ?FORALL(
+    {Message, Secret}, {binary(), binary(32)},
+    Message =:= decode(encode(Message, Secret), Secret)
+  ).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%%%%
